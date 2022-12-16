ad34f3cc16f2a40f9f0b277f402c8aea2b6f88de
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2022-12-17 00:02:23 +0100

    Completed the FacesAPI and Test Project

---
+++ FacesAndFaces/FacesAPITest/ImageUtility.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace FacesAPITest
+{
+    public class ImageUtility
+    {
+       public byte[] ConvertToBytes(string imagePath)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+
+            using(FileStream stream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+            {
+                stream.CopyTo(memoryStream);
+            }
+            var bytes = memoryStream.ToArray();
+            return bytes;
+        }
+
+        public void FromBytesToImage(byte[] imageBytes, string fileName)
+        {
+            using(var ms = new MemoryStream(imageBytes))
+            {
+                Image img  = Image.FromStream(ms);
+                img.Save(fileName+".jpg", ImageFormat.Jpeg);
+            }
+        }
+    }
+}
---
+++ FacesAndFaces/FacesAPITest/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FacesAPITest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var imagePath = @"C:\Users\USER\source\repos\.NetMicroservices\FacesAndFaces\FacesAPITest\oscars-2017.jpg";
+            var imagePath = @"oscars-2017.jpg";
+            var urlAddress = "http://localhost:6000/api/Faces";
+            ImageUtility imgUtil = new ImageUtility();
+
+            var bytes = imgUtil.ConvertToBytes(imagePath);
+
+            List<byte[]> faceList = null;
+
+            Tuple<List<byte[]>> facesResponse = null;
+
+
+
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            using(var httpClient = new HttpClient())
+            {
+                using(var response = await httpClient.PostAsync(urlAddress, byteContent))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    facesResponse = JsonConvert.DeserializeObject<Tuple<List<byte[]>>>(apiResponse);
+                }
+            }
+
+            if(facesResponse.Item1.Count > 0)
+            {
+                for(int i = 0; i < facesResponse.Item1.Count; i++)
+                {
+                    imgUtil.FromBytesToImage(facesResponse.Item1[i], "faces" + i);
+                }
+            }
+        }
+    }
+}
